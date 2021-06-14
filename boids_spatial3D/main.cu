f16c60465f6a388e5b0c1242f64175286ae3a09d
Robert Chisholm <R.Chisholm@sheffield.ac.uk>
2021-06-14 12:45:34 +0100

    Double pop size for better occupancy on v100

--- boids_spatial3D/main.cu
+++ boids_spatial3D/main.cu
@@ -295,11 +295,11 @@ int main(int argc, const char ** argv) {
         EnvironmentDescription &env = model.Environment();
 
         // Population size to generate, if no agents are loaded from disk
-        env.newProperty("POPULATION_TO_GENERATE", 81920u);
+        env.newProperty("POPULATION_TO_GENERATE", 163840u);
 
-        // Environment Bounds
-        env.newProperty("MIN_POSITION", -0.7f);
-        env.newProperty("MAX_POSITION", +0.7f);
+        // Environment Bounds 
+        env.newProperty("MIN_POSITION", -0.9f);
+        env.newProperty("MAX_POSITION", +0.9f);
 
         // Initialisation parameter(s)
         env.newProperty("MAX_INITIAL_SPEED", 1.0f);
